fc8aa8361517d4f497a158578b7d370ac9db0244
Alexandre Nguyen <alexandre2.nguyen@epitech.eu>
2019-11-07 11:24:38 +0100

    server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/quokka-proto'));
+app.use(express.static(__dirname + '/dist/Quokka-Proto'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/quokka-proto/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Quokka-Proto/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
